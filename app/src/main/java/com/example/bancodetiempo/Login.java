7b6b9a82887765066373a4612799ab907019c542
OpaqueBlock <sushitofox@gmail.com>
2021-09-23 00:33:04 -0500

    Main activity main change.

--- app/src/main/java/com/example/bancodetiempo/Login.java
+++ app/src/main/java/com/example/bancodetiempo/Login.java
@@ -5,12 +5,12 @@ import android.view.View;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
     public void goRegister(View view) {
