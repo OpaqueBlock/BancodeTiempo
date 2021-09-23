905e052ff7229a4354290fe6992d66ce1c8f3e3d
Angel009 <angelus007.ans@gmail.com>
2021-09-23 09:05:11 -0500

    branch creation

---
+++ app/src/main/java/com/example/bancodetiempo/listadoOfertas.java
@@ -0,0 +1,37 @@
+package com.example.bancodetiempo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class listadoOfertas extends AppCompatActivity {
+
+    List<ElementoListado> ofertas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_ofertas);
+
+        init();
+    }
+
+    public void init(){
+        ofertas=new ArrayList<>();
+        ofertas.add(new ElementoListado("Pedro", "A", "#ff0000"));
+        ofertas.add(new ElementoListado("Sandra", "A", "#ff0000"));
+        ofertas.add(new ElementoListado("Pedro", "A", "#ffcd00"));
+        ofertas.add(new ElementoListado("Sandra", "A", "#ffcd00"));
+
+        AdaptadorListado lista=new AdaptadorListado(ofertas, this);
+        RecyclerView recyclerView=findViewById(R.id.listaOfertas);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(lista);
+    }
+}
