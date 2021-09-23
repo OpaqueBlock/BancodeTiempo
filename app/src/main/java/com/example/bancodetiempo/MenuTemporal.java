5a9bc7e3524d0c4956471d8bb7c72fde76425c68
AdrianTorresG <atg292001@hotmail.com>
2021-09-23 13:56:05 -0500

    Ajustes ligado

--- app/src/main/java/com/example/bancodetiempo/MenuTemporal.java
+++ app/src/main/java/com/example/bancodetiempo/MenuTemporal.java
@@ -2,7 +2,9 @@ package com.example.bancodetiempo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuTemporal extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MenuTemporal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_temporal);
+    }
+
+    public void goAjustes (View view) {
+        Intent miIntent = new Intent(MenuTemporal.this, Ajustes.class);
+        startActivity(miIntent);
     }
 }
